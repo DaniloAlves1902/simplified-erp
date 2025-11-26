55c403d924672a81022c0cd9c8902362c0282304
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-11-26 08:43:21 -0300

    feat: add stock movement domain entity

---
+++ src/main/java/com/danilo_alves/simplified_erp/domain/entity/stock/StockMovementDomain.java
@@ -0,0 +1,25 @@
+package com.danilo_alves.simplified_erp.domain.entity.stock;
+
+import com.danilo_alves.simplified_erp.domain.entity.product.ProductDomain;
+import com.danilo_alves.simplified_erp.domain.entity.user.UserDomain;
+import com.danilo_alves.simplified_erp.domain.enums.stock.MovementDomain;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockMovementDomain {
+    private Long id;
+    private ProductDomain product;
+    private Integer quantityChanged;
+    private OffsetDateTime timeStamp;
+    private UserDomain changedBy;
+    private MovementDomain movementType;
+
+}
---
+++ src/main/java/com/danilo_alves/simplified_erp/domain/enums/stock/MovementDomain.java
@@ -0,0 +1,14 @@
+package com.danilo_alves.simplified_erp.domain.enums.stock;
+
+public enum MovementDomain {
+    SALE,
+    ENTRY,
+    EXIT,
+    RETURN,
+    ADJUSTMENT,
+    TRANSFER,
+    PURCHASE,
+    PRODUCTION,
+    CONSUMPTION,
+    OTHER
+}
