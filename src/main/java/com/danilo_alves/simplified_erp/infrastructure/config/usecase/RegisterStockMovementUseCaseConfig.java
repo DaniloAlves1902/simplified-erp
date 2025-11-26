4e652237f24b5c992ad35c35e9f4a4851c33a782
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-11-26 14:46:45 -0300

    feat: add register stock movement use case config

---
+++ src/main/java/com/danilo_alves/simplified_erp/infrastructure/config/usecase/RegisterStockMovementUseCaseConfig.java
@@ -0,0 +1,15 @@
+package com.danilo_alves.simplified_erp.infrastructure.config.usecase;
+
+import com.danilo_alves.simplified_erp.application.gateway.stock.StockGateway;
+import com.danilo_alves.simplified_erp.application.usecase.stock.impl.RegisterStockMovementUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RegisterStockMovementUseCaseConfig {
+
+    @Bean
+    public RegisterStockMovementUseCase registerStockMovementUseCase(StockGateway stockGateway) {
+        return new RegisterStockMovementUseCase(stockGateway);
+    }
+}
