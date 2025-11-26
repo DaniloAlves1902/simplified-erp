3c90c6aeac32a4129f639641d2975eae9a5eb7eb
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-11-26 14:42:54 -0300

    feat: add stock movement repository jpa

---
+++ src/main/java/com/danilo_alves/simplified_erp/infrastructure/persistence/repository/StockMovementRepositoryJPA.java
@@ -0,0 +1,10 @@
+package com.danilo_alves.simplified_erp.infrastructure.persistence.repository;
+
+import com.danilo_alves.simplified_erp.infrastructure.persistence.entity.stock.StockMovementEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StockMovementRepositoryJPA extends JpaRepository<StockMovementEntity, Long> {
+}
