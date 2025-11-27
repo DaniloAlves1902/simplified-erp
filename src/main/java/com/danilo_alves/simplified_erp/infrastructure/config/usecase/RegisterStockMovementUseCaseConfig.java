ca5739f30890d74f4aa1fe7559c8363f790bb2b1
DaniloAlves1902 <danilo.alvess1902@gmail.com>
2025-11-27 10:37:28 -0300

    feat: add bean at get all stock movement

--- src/main/java/com/danilo_alves/simplified_erp/infrastructure/config/usecase/RegisterStockMovementUseCaseConfig.java
+++ src/main/java/com/danilo_alves/simplified_erp/infrastructure/config/usecase/RegisterStockMovementUseCaseConfig.java
@@ -1,6 +1,7 @@
 package com.danilo_alves.simplified_erp.infrastructure.config.usecase;
 
 import com.danilo_alves.simplified_erp.application.gateway.stock.StockGateway;
+import com.danilo_alves.simplified_erp.application.usecase.stock.impl.GetAllStockMovementUseCase;
 import com.danilo_alves.simplified_erp.application.usecase.stock.impl.RegisterStockMovementUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,10 @@ public class RegisterStockMovementUseCaseConfig {
     @Bean
     public RegisterStockMovementUseCase registerStockMovementUseCase(StockGateway stockGateway) {
         return new RegisterStockMovementUseCase(stockGateway);
+    }
+
+    @Bean
+    GetAllStockMovementUseCase getAllStockMovementUseCase(StockGateway stockGateway) {
+        return new GetAllStockMovementUseCase(stockGateway);
     }
 }
